2266cfc5c6b29dab999ac995d7ae28b26a22e458
icaro089 <45367412+icaro089@users.noreply.github.com>
2020-07-22 15:17:37 -0300

    Created a vehicle list page

--- vehicle_CRUD/app/views.py
+++ vehicle_CRUD/app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Vehicle
 
 # Create your views here.
+
+
+def vehicle_list(request, template_name='vehicle_list.html'):
+    vehicle = Vehicle.objects.all()
+    vehicles = {'list': vehicle}
+    return render(request,template_name, vehicles)

04f9b2a63b1ba2f02997c84fb430e28821fdd7d2
icaro089 <45367412+icaro089@users.noreply.github.com>
2020-07-22 18:26:21 -0300

    Created search filters

--- vehicle_CRUD/app/views.py
+++ vehicle_CRUD/app/views.py
@@ -1,3 +1,5 @@
+from django.core import paginator
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render
 from .models import Vehicle
 
@@ -5,6 +7,22 @@ from .models import Vehicle
 
 
 def vehicle_list(request, template_name='vehicle_list.html'):
-    vehicle = Vehicle.objects.all()
+    query = request.GET.get("search", '')
+    page = request.GET.get("page", '')
+    order = request.GET.get("order", '')
+    if query:
+        vehicle = Vehicle.objects.filter(model__icontains=query)
+    else:
+        try:
+            if order:
+                vehicle = Vehicle.objects.all().order_by(order)
+            else:
+                vehicle = Vehicle.objects.all()
+            vehicle = Paginator(vehicle, 1)
+            vehicle = vehicle.page(page)
+        except PageNotAnInteger:
+            vehicle = vehicle.page(1)
+        except EmptyPage:
+            vehicle = paginator.page(paginator.num_pages)
     vehicles = {'list': vehicle}
     return render(request,template_name, vehicles)

69b03db3af0de823d53b600153f0b9e86d0a1975
icaro089 <45367412+icaro089@users.noreply.github.com>
2020-07-22 19:21:06 -0300

    Implemented filter and pagination

--- vehicle_CRUD/app/views.py
+++ vehicle_CRUD/app/views.py
@@ -8,21 +8,41 @@ from .models import Vehicle
 
 def vehicle_list(request, template_name='vehicle_list.html'):
     query = request.GET.get("search", '')
-    page = request.GET.get("page", '')
+    #page = request.GET.get("page", '')
+    page_number = request.GET.get('page', '')
     order = request.GET.get("order", '')
     if query:
         vehicle = Vehicle.objects.filter(model__icontains=query)
+        base_url = '/vehicles/list/?search=' + query + '&'
     else:
-        try:
-            if order:
-                vehicle = Vehicle.objects.all().order_by(order)
-            else:
-                vehicle = Vehicle.objects.all()
-            vehicle = Paginator(vehicle, 1)
-            vehicle = vehicle.page(page)
-        except PageNotAnInteger:
-            vehicle = vehicle.page(1)
-        except EmptyPage:
-            vehicle = paginator.page(paginator.num_pages)
-    vehicles = {'list': vehicle}
+        if order:
+            vehicle = Vehicle.objects.all().order_by(order)
+            base_url = '/vehicles/list/?order=' + order + '&'
+        else:
+            vehicle = Vehicle.objects.all()
+            base_url = '/vehicles/list/?'
+    paginate_result = do_paginate(vehicle, page_number)
+    vehicle = paginate_result[0]
+    paginator = paginate_result[1]
+    #base_url = '/vehicles/list/?order='+order+'&'
+ #   try:
+  #      vehicle = Paginator(vehicle, 1)
+   #     vehicle = vehicle.page(page)
+    #except PageNotAnInteger:
+     #   vehicle = vehicle.page(1)
+    #except EmptyPage:
+      #  vehicle = paginator.page(paginator.num_pages)
+    vehicles = {'list': vehicle, 'paginator': paginator, 'base_url': base_url}
     return render(request,template_name, vehicles)
+
+def do_paginate(data_list, page_number):
+    ret_data_list = data_list
+    result_per_page = 1
+    paginator = Paginator(ret_data_list, result_per_page)
+    try:
+        ret_data_list = paginator.page(page_number)
+    except EmptyPage:
+        ret_data_list = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        ret_data_list = paginator.page(1)
+    return [ret_data_list, paginator]
